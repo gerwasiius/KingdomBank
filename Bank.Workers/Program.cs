91bb0ce2f7f6733cce57f43d51d8ff86a2aae0b2
gerwasius <gerwasius007@hotmail.com>
2025-09-02 21:24:55 +0200

    Zavrseno kreiranje JWKS-a.

--- Bank.Workers/Program.cs
+++ Bank.Workers/Program.cs
@@ -4,10 +4,12 @@ using Bank.Infrastructure;
 using Bank.Infrastructure.Security;
 using Bank.Workers;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using Serilog;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+// Serilog
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .Enrich.FromLogContext()
@@ -17,25 +19,40 @@ Log.Logger = new LoggerConfiguration()
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(dispose: true);
 
-// EF (DB-first)
+// EF Core (SQL Server) – koristi ConnectionStrings:Sql
 builder.Services.AddDbContext<BankDbContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("Sql")));
+    opt.UseSqlServer(
+        builder.Configuration.GetConnectionString("Sql")
+        ?? builder.Configuration["ConnectionStrings:Sql"] // fallback na env var
+    )
+);
 
-// Generatori ključeva
+// Generator ključeva
 builder.Services.AddSingleton<IKeyGenerator, RsaKeyGenerator>();
 builder.Services.AddSingleton<IKeyGenerator, EcdsaKeyGenerator>();
 
-// Vault (DEV)
+// Vault (FileKeyVault radi s /keys; vidi compose volumen)
 builder.Services.AddSingleton<FileKeyVault>();
 builder.Services.AddSingleton<IKeyVault>(sp => sp.GetRequiredService<FileKeyVault>());
 
 // Repo (EF)
 builder.Services.AddScoped<IKeyRotationRepository, KeyRotationEfRepository>();
 
-// Worker
+// Hosted service (singleton) – NE injektuje repo direktno
 builder.Services.AddHostedService<KeyRotationWorker>();
 
 var host = builder.Build();
-try { Log.Information("Starting worker host"); host.Run(); }
-catch (Exception ex) { Log.Fatal(ex, "Host terminated unexpectedly"); }
-finally { Log.CloseAndFlush(); }
+
+try
+{
+    Log.Information("Starting worker host");
+    host.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
